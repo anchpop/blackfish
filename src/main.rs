af60467ae47883dbf9915ae5facc9ad1c56ff269
Andre Popovitch <andre@popovit.ch>
2021-02-20 03:15:01 -0500

    added positioning functin

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, ops::Neg};
 
 use bevy::prelude::*;
 use ndarray::{arr2, Array2};
@@ -45,6 +45,7 @@ fn main() {
         .add_startup_stage("game_setup", SystemStage::single(spawn_main_tile.system())) // Adding a stage lets us access resources (in this case, materials) created in the previous stage
         .add_plugins(DefaultPlugins)
         .add_system(size_scaling.system())
+        .add_system(positioning.system())
         .run();
 }
 
@@ -85,15 +86,40 @@ fn size_scaling(
     windows: Res<Windows>,
     mut q: Query<(&TileSize, &mut Sprite)>,
     tilemap: Res<TilemapWorld>,
-    time: Res<Time>,
 ) {
-    let (width, height) = tilemap.map.dim();
-
+    let (arena_tiles_wide, arena_tiles_tall) = tilemap.map.dim();
     let window = windows.get_primary().unwrap();
+
     for (sprite_size, mut sprite) in q.iter_mut() {
         sprite.size = Vec2::new(
-            sprite_size.width as f32 / width as f32 * window.width() as f32,
-            sprite_size.height as f32 / height as f32 * window.height() as f32,
+            sprite_size.width as f32 / arena_tiles_wide as f32 * window.width() as f32,
+            sprite_size.height as f32 / arena_tiles_tall as f32 * window.height() as f32,
+        );
+    }
+}
+
+fn positioning(
+    windows: Res<Windows>,
+    mut q: Query<(&TilePosition, &mut Transform)>,
+    tilemap: Res<TilemapWorld>,
+) {
+    let (arena_tiles_wide, arena_tiles_tall) = tilemap.map.dim();
+    let window = windows.get_primary().unwrap();
+
+    fn convert(pos: f32, bound_window: f32, bound_game: f32) -> f32 {
+        let tile_size = bound_window / bound_game;
+        pos / bound_game * bound_window - (bound_window / 2.) + (tile_size / 2.)
+    }
+
+    for (pos, mut transform) in q.iter_mut() {
+        transform.translation = Vec3::new(
+            convert(pos.x as f32, window.width() as f32, arena_tiles_wide as f32),
+            convert(
+                pos.y as f32,
+                window.height() as f32,
+                arena_tiles_tall as f32,
+            ),
+            0.0,
         );
     }
 }
