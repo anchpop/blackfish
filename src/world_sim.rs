7d12a1ce4df09f1880e9c5b66aa075e57e01370e
Andre Popovitch <andre@popovit.ch>
2021-02-21 10:46:12 -0500

    fixed the thing that was bugging me last night

--- src/world_sim.rs
+++ src/world_sim.rs
@@ -2,6 +2,6 @@ use crate::types::*;
 
 pub fn sim(prog: TilemapProgram) -> TilemapWorld {
     TilemapWorld {
-        map: prog.map.map(|a| a.clone().map(TileWorld::Prog)),
+        map: prog.map.mapv(|a| a.map(TileWorld::Prog)),
     }
 }
