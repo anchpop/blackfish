77345a39664c1dbaa5fe439c2b0a91dbe9e3ab97
Andre Popovitch <andre@popovit.ch>
2021-03-17 15:58:00 -0400

    started working on midi 8)

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,19 @@ use std::time::Duration;
 use velcro::btree_map;
 use velcro::hash_map;
 
+use std::error::Error;
+use std::io::{stdin, stdout, Write};
+use std::thread::sleep;
+
+use midir::{MidiOutput, MidiOutputConnection, MidiOutputPort};
+
+use std::sync::Mutex;
+/*
+fn run() -> Result<(), Box<dyn Error>> {
+
+    Ok(())
+}*/
+
 #[derive(Debug, Clone)]
 struct ClockIncrementTimer(Timer);
 impl Default for ClockIncrementTimer {
@@ -30,9 +43,15 @@ struct CurrentClock(i32);
 
 fn main() {
     App::build()
+        .add_startup_system(get_midi_ports.system())
         .add_startup_system(setup.system())
         .add_startup_system(create_map.system())
-        .add_startup_stage("game_setup", SystemStage::single(spawn_main_tile.system())) // Adding a stage lets us access resources (in this case, materials) created in the previous stage
+        // Adding a stage lets us access resources (in this case, materials) created in the previous stage
+        .add_startup_stage("game_setup", SystemStage::single(spawn_main_tile.system()))
+        .add_startup_stage(
+            "play_notes_test",
+            SystemStage::single(play_notes_test.system()),
+        )
         .add_plugins(DefaultPlugins)
         .add_system(clock_increment.system())
         .add_system(size_scaling.system())
@@ -106,6 +125,77 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
         .cloned()
         .collect(),
     });
+}
+
+fn get_midi_ports(commands: &mut Commands) {
+    fn run(commands: &mut Commands) -> Result<(), Box<dyn Error>> {
+        let midi_out = MidiOutput::new("Blackfish Midi Output")?;
+
+        // Get an output port (read from console if multiple are available)
+        let out_ports = midi_out.ports();
+        let out_port: &MidiOutputPort = match out_ports.len() {
+            0 => return Err("no output port found".into()),
+            1 => {
+                println!(
+                    "Choosing the only available output port: {}",
+                    midi_out.port_name(&out_ports[0]).unwrap()
+                );
+                &out_ports[0]
+            }
+            _ => {
+                println!("\nAvailable output ports:");
+                for (i, p) in out_ports.iter().enumerate() {
+                    println!("{}: {}", i, midi_out.port_name(p).unwrap());
+                }
+                print!("Please select output port: ");
+                stdout().flush()?;
+                let mut input = String::new();
+                stdin().read_line(&mut input)?;
+                out_ports
+                    .get(input.trim().parse::<usize>()?)
+                    .ok_or("invalid output port selected")?
+            }
+        };
+        println!("\nOpening connection");
+        let mut conn_out = midi_out.connect(out_port, "midir-test")?;
+
+        commands.insert_resource(Mutex::new(conn_out));
+
+        Ok(())
+    }
+
+    match run(commands) {
+        Ok(_) => (),
+        Err(err) => println!("Error: {}", err),
+    }
+}
+
+fn play_notes_test(commands: &mut Commands, mut conn_out: Res<Mutex<midir::MidiOutputConnection>>) {
+    let mut conn_out = conn_out.lock().unwrap();
+    println!("Connection open. Listen!");
+    {
+        // Define a new scope in which the closure `play_note` borrows conn_out, so it can be called easily
+        let mut play_note = |note: u8, duration: u64| {
+            const NOTE_ON_MSG: u8 = 0x90;
+            const NOTE_OFF_MSG: u8 = 0x80;
+            const VELOCITY: u8 = 0x64;
+            // We're ignoring errors in here
+            let _ = conn_out.send(&[NOTE_ON_MSG, note, VELOCITY]);
+            sleep(Duration::from_millis(duration * 150));
+            let _ = conn_out.send(&[NOTE_OFF_MSG, note, VELOCITY]);
+        };
+
+        sleep(Duration::from_millis(4 * 150));
+
+        play_note(66, 4);
+        play_note(65, 3);
+        play_note(63, 1);
+        play_note(61, 6);
+        play_note(59, 2);
+        play_note(58, 4);
+        play_note(56, 4);
+        play_note(54, 4);
+    }
 }
 
 fn create_map(commands: &mut Commands) {
